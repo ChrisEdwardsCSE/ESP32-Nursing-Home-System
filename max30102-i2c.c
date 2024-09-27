f80438c7077d82b0b57f3644e6431e12699fd0ce
Christopher Edwards <66543275+ChrisEdwardsCSE@users.noreply.github.com>
2024-09-26 18:09:40 -0700

    peripherals work by themselves

---
+++ max30102-i2c.c
@@ -0,0 +1,195 @@
+/**
+ * SCL - <= 400kHz
+ * 8b transactions.
+ * address = 0xa7 << 1
+ * "fixed i2c addresses": 0xae for write, 0xaf for read
+ *
+ *
+ * Power Up - after power up, interrupt occurs to alert that
+ * MAX30102 ready. then read the int register to clear it
+ *
+ * Interrupts - read the interrupt source reg or the data reg to clear
+ * an int
+ * interrupts are IDLE-HIGH
+ */
+
+// https://github.com/eepj/stm32-max30102/blob/master/max30102_for_stm32_hal.c
+#include <stdio.h>
+#include <stdint.h>
+#include <string.h>
+#include "hal/spi_types.h"
+
+#include "driver/i2c_master.h"
+#include "driver/gpio.h"
+#include "sdkconfig.h"
+
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+
+
+
+#define MAX_INT_SOURCE_1_REG 0x00
+#define MAX_LED1_PA_REG 0x0c
+#define MAX_LED2_PA_REG 0x0d
+#define MAX_MODE_CONFIG_REG 0x09
+#define MAX_CONFIG_SPO2_REG 0x0a
+#define MAX_FIFO_CONFIG_REG 0x08
+#define MAX_FIFO_DATA_REG 0x07
+
+#define MAX_FIFO_WRITE_PTR 0x04
+#define MAX_FIFO_OVF_CTR 0x05
+#define MAX_FIFO_READ_PTR 0x06
+
+#define MAX_DEBUG 1
+
+void max30102_config(i2c_master_dev_handle_t master_dev_handle);
+void max30102_init(i2c_master_dev_handle_t master_dev_handle);
+void max30102_fifo_read(i2c_master_dev_handle_t master_dev_handle, uint32_t* hr_sample, uint32_t* ir_sample);
+void max30102_fifo_init(i2c_master_dev_handle_t master_dev_handle);
+
+void max30102_int_handle(i2c_master_dev_handle_t master_dev_handle)
+{
+#if MAX_DEBUG
+    printf("max30102_int_handle() called\n");
+#endif
+    /* Read interrupt registers; Clears all interrupts */
+    uint8_t int_source_buf[1] = {
+        MAX_INT_SOURCE_1_REG};
+    uint8_t read_int_buf[2]; // Read both Interrupt Status Registers 1 & 2
+    i2c_master_transmit_receive(master_dev_handle, int_source_buf, 1,
+                                read_int_buf, 2, 100);
+    // if PWR_RDY interrupt
+    if (read_int_buf[0] & 0x1)
+    {
+#if MAX_DEBUG
+        printf("max30102_int_handle(): power ready: 0x%X, 0x%X\n", read_int_buf[0], read_int_buf[1]);
+#endif
+    }
+}
+
+void max30102_init(i2c_master_dev_handle_t master_dev_handle)
+{
+#if MAX_DEBUG
+    printf("max30102_init() called\n");
+#endif
+    /* Set the mode to HR (heart rate) mode */
+    uint8_t mode_buf[2] = {
+        MAX_MODE_CONFIG_REG,
+        (uint8_t)0x02 // HR mode
+    };
+
+    i2c_master_transmit(master_dev_handle, mode_buf, 2, 100);
+
+#if MAX_DEBUG
+    uint8_t rx_mode_buf[1];
+    i2c_master_transmit_receive(master_dev_handle, mode_buf, 1,
+                                rx_mode_buf, 1, 100);
+    printf("MODE_CONF: 0x%X\n", rx_mode_buf[0]);
+#endif
+
+    max30102_config(master_dev_handle);
+
+    max30102_fifo_init(master_dev_handle);
+}
+
+/**
+ * Configure sampling stuff. go for 100 samples/s and 15 bit resolution
+ */
+void max30102_config(i2c_master_dev_handle_t master_dev_handle)
+{
+#if MAX_DEBUG
+    printf("max301020_config() called\n");
+#endif
+    /* Set LED Pulse Amplitude of Red LED */
+    uint8_t led1_pa_buf[2] = {
+        MAX_LED1_PA_REG,
+        0x24 // 7mA
+    };
+    i2c_master_transmit(master_dev_handle, led1_pa_buf, 2, 100);
+
+#if MAX_DEBUG
+
+    uint8_t rx_buf[1];
+    i2c_master_transmit_receive(master_dev_handle, led1_pa_buf,
+                                1, rx_buf, 1, 100);
+    vTaskDelay(10 / portTICK_PERIOD_MS);
+    printf("LED1 config: 0x%X\n", rx_buf[0]);
+#endif
+
+    /* Set ADC configurations */
+    uint8_t config_adc_buf[2] = {
+        MAX_CONFIG_SPO2_REG,
+        (uint8_t)((0b01 << 5) | (0b001 << 2) | (0b11)) // 4096nA range, 100 samples/s, 411us pulse width
+    };
+
+    i2c_master_transmit(master_dev_handle, config_adc_buf, 2, 100);
+
+#if MAX_DEBUG
+    i2c_master_transmit_receive(master_dev_handle, config_adc_buf,
+                                1, rx_buf, 1, 100);
+    vTaskDelay(10 / portTICK_PERIOD_MS);
+    printf("SP02 config: 0x%X\n", rx_buf[0]);
+#endif
+}
+
+/**
+ * So FIFO works like, 32 spots for 32 samples of data.
+ * A channel provides 3B of data. There are 2 channels: IR & RED.
+ * If we're using both, that means a sample can be 2(channels) * 3(Bytes) = 6B per sample
+ * The 3B are actually only 18b max, from 15-18b ADC resolutions ([23:18] aren't used)
+ */
+
+/**
+ * Initialize the FIFO with configurations
+ * Gets called upon Power-Up and Brownout
+ */
+void max30102_fifo_init(i2c_master_dev_handle_t master_dev_handle)
+{
+    // config fifo
+    // enable fifo interrupts
+    // reset the write ptr, read ptr, ovf counter
+    // clear any interrupts
+    uint8_t rx_buf[2];
+    /* Set FIFO configurations */
+    uint8_t tx_buf[2] = {
+        MAX_FIFO_CONFIG_REG,
+        (uint8_t)((0b010 << 5) | (1 << 4)) // // 4 samples averaged per FIFO sample; FIFO rollover enabled
+    };
+    i2c_master_transmit(master_dev_handle, tx_buf, 2, 100);
+
+#if MAX_DEBUG
+    printf("max30102_fifo_init() called\n");
+    i2c_master_transmit_receive(master_dev_handle, tx_buf,
+                                1, rx_buf, 1, 100);
+    vTaskDelay(10 / portTICK_PERIOD_MS);
+    printf("FIFO config: 0x%X\n", rx_buf[0]);
+#endif
+
+    /* Reset FIFO values u*/
+    tx_buf[0] = MAX_FIFO_WRITE_PTR;
+    tx_buf[1] = 0x00;
+    i2c_master_transmit(master_dev_handle, tx_buf, 2, 100);
+
+    tx_buf[0] = MAX_FIFO_OVF_CTR;
+    i2c_master_transmit(master_dev_handle, tx_buf, 2, 100);
+
+    tx_buf[0] = MAX_FIFO_READ_PTR;
+    i2c_master_transmit(master_dev_handle, tx_buf, 2, 100);
+
+    /* Clear FIFO interrupts by reading source */
+    tx_buf[0] = MAX_INT_SOURCE_1_REG;
+    i2c_master_receive(master_dev_handle, tx_buf, 1, 100);
+}
+
+uint8_t rx_data_buf[6];
+void max30102_fifo_read(i2c_master_dev_handle_t master_dev_handle, uint32_t* hr_sample, uint32_t* ir_sample)
+{
+    printf("&hr_sample %p, ir_sample %p\n", hr_sample, ir_sample);
+    rx_data_buf[0] = MAX_FIFO_DATA_REG;
+
+    i2c_master_transmit(master_dev_handle, rx_data_buf, 1, 100); // just sent MAX_FIFO_DATA_REG
+    vTaskDelay(5 / portTICK_PERIOD_MS);
+    i2c_master_receive(master_dev_handle, rx_data_buf, 6, 100); // read 6 bytes into buf
+    *ir_sample = (uint32_t)((rx_data_buf[0] << 16) | (rx_data_buf[1] << 8) | (rx_data_buf[2])) & 0x3ffff;
+    *hr_sample = (uint32_t)((rx_data_buf[3] << 16) | (rx_data_buf[4] << 8) | (rx_data_buf[5])) & 0x3ffff;
+}
--- nursing-periphs.c
+++ nursing-periphs.c
@@ -16,18 +16,54 @@
 #include "driver/i2c_master.h"
 #include "driver/i2c_types.h"
 #include "driver/i2c_slave.h"
-#include "drivers/src/adxl345-i2c.c"
 
 #include "inttypes.h"
 
-#define ADXL_IO_INT1 GPIO_NUM_5
-#define ADXL_IO_INT2 GPIO_NUM_6
+#include "drivers/Src/adxl345-i2c.c"
+#include "drivers/Src/max30102-i2c.c"
+#include "drivers/Src/algorithm_by_RF.c"
+
+#define ADXL_IO_INT1    GPIO_NUM_5
+#define ADXL_IO_INT2    GPIO_NUM_6
+#define MAX_IO_INT      GPIO_NUM_7
 
 
 bool yellow_led_val = 1;
 bool green_led_val = 1;
 
-i2c_master_dev_handle_t master_dev_handle;
+i2c_master_dev_handle_t adxl_master_dev_handle;
+i2c_master_bus_handle_t adxl_master_bus_handle;
+
+i2c_master_dev_handle_t max_master_dev_handle;
+
+
+#define ADXL_TESTING 1
+#define MAX_TESTING 0
+
+TaskHandle_t ISR = NULL;
+
+uint8_t read_data[1] = {
+    0x32, // DATA_X0 register
+};
+size_t read_data_len = 1;
+uint8_t readings_buf[6];
+size_t readings_buf_len = 6;
+int16_t x, y, z;
+
+void task1(void* arg)
+{
+    while(1)
+    {
+        i2c_master_transmit_receive(adxl_master_dev_handle, read_data, 1, 
+                        readings_buf, 6, 100);
+        x = readings_buf[0] | readings_buf[1] << 8;
+        y = readings_buf[2] | readings_buf[3] << 8;
+        z = readings_buf[4] | readings_buf[5] << 8;
+        printf("x: %d, y: %d, z: %d\n", x,y,z);
+        vTaskDelay(500/portTICK_PERIOD_MS);
+    }
+
+}
 
 /**
  * SINGLE_TAP event on INT1 (yellow)
@@ -35,42 +71,43 @@ i2c_master_dev_handle_t master_dev_handle;
  */
 static void IRAM_ATTR adxl_int1_isr_handler(void *arg)
 {
-    gpio_set_level(GPIO_NUM_1, yellow_led_val);
-    yellow_led_val = !yellow_led_val;
+    // printf("int1 triggered\n");
+    gpio_set_level(GPIO_NUM_1, green_led_val);
+    green_led_val = !green_led_val;
     // Read out and clear the SINGLE_TAP interrupt
-    adxl345_int_tap_clear(master_dev_handle);
+    adxl345_int_tap_clear(adxl_master_dev_handle, adxl_master_bus_handle);
 }
 
 static void IRAM_ATTR adxl_int2_isr_handler(void *arg)
 {
-    gpio_set_level(GPIO_NUM_2, green_led_val);
-    green_led_val = !green_led_val;
-
-
+    // printf("int2 triggered\n");
+    // gpio_set_level(GPIO_NUM_2, yellow_led_val);
+    // yellow_led_val = !yellow_led_val;
+    adxl345_int_tap_clear(adxl_master_dev_handle, adxl_master_bus_handle);
 }
+
 
 void app_main()
 {
-
     /*** Configure GPIO Pins for ADXL INT1 and INT 2 ***/
     gpio_reset_pin(ADXL_IO_INT1);
     gpio_reset_pin(ADXL_IO_INT2);
 
-    gpio_config_t adxl_int1_config = {
-        .pin_bit_mask = (1 << GPIO_NUM_5),
+    gpio_config_t adxl_int1_gpio_config = {
+        .pin_bit_mask = (1 << ADXL_IO_INT1),
         .mode = GPIO_MODE_INPUT,
         .pull_down_en = 1,
         .intr_type = GPIO_INTR_POSEDGE, // rising edge interrupt
     };
-    gpio_config(&adxl_int1_config);
+    gpio_config(&adxl_int1_gpio_config);
 
-    gpio_config_t adxl_int2_config = {
-        .pin_bit_mask = (1 << GPIO_NUM_6),
+    gpio_config_t adxl_int2_gpio_config = {
+        .pin_bit_mask = (1 << ADXL_IO_INT2),
         .mode = GPIO_MODE_INPUT,
         .pull_down_en = 1,
         .intr_type = GPIO_INTR_POSEDGE, // rising edge interrupt
     };
-    gpio_config(&adxl_int2_config);
+    gpio_config(&adxl_int2_gpio_config);
 
     gpio_install_isr_service(0);
     gpio_isr_handler_add(ADXL_IO_INT1, adxl_int1_isr_handler, NULL);
@@ -82,15 +119,16 @@ void app_main()
     gpio_intr_enable(ADXL_IO_INT1);
     gpio_intr_enable(ADXL_IO_INT2);
 
-    /* TEMP<^> - LED gpio pins initialization*/
+    /* *********** TESTING ********* LED gpio pins initialization*/
     gpio_set_direction(GPIO_NUM_1, GPIO_MODE_OUTPUT);
     gpio_set_pull_mode(GPIO_NUM_1, GPIO_PULLUP_PULLDOWN);
 
     gpio_set_direction(GPIO_NUM_2, GPIO_MODE_OUTPUT);
     gpio_set_pull_mode(GPIO_NUM_2, GPIO_PULLUP_PULLDOWN);
+    /* ***** TESTING ******* */
 
-    /*** Configure I2C ***/
-    i2c_master_bus_config_t master_bus_config = {
+    /*** Configure I2C for ADXL345 ***/
+    i2c_master_bus_config_t adxl_master_bus_config = {
         .i2c_port = -1,
         .scl_io_num = GPIO_NUM_15,
         .sda_io_num = GPIO_NUM_16,
@@ -100,49 +138,25 @@ void app_main()
         .flags = {1} // enable pull up
     };
 
-    i2c_master_bus_handle_t master_bus_handle;
-    ESP_ERROR_CHECK(i2c_new_master_bus(&master_bus_config, &master_bus_handle));
+    
+    ESP_ERROR_CHECK(i2c_new_master_bus(&adxl_master_bus_config, &adxl_master_bus_handle));
 
     /* Configure device */
-    i2c_device_config_t device_config = {
+    i2c_device_config_t adxl_device_config = {
         .dev_addr_length = I2C_ADDR_BIT_7,
         .device_address = 0x53, // address when SDO cnctd to GND
         .scl_speed_hz = 100000  // ADXL either 100kHz or 400kHz
     };
 
-    i2c_master_bus_add_device(master_bus_handle, &device_config, &master_dev_handle);
+    i2c_master_bus_add_device(adxl_master_bus_handle, &adxl_device_config, &adxl_master_dev_handle);
 
-    adxl345_init(master_dev_handle);
+    adxl345_init(adxl_master_dev_handle);
 
-    // adxl345_int_tap_clear(master_dev_handle);
+    adxl345_interrupt_init(adxl_master_dev_handle, adxl_master_bus_handle, 2500, 100, 800, 200);
 
-    adxl345_interrupt_init(master_dev_handle, 2000, 100, 800, 200);
+    adxl345_int_tap_clear(adxl_master_dev_handle, adxl_master_bus_handle);
 
-     /* Take readings */
-    uint8_t read_data[1] = {
-        0x32, // DATA_X0 register
-    };
-    size_t read_data_len = 1;
-    uint8_t readings_buf[6];
-    size_t readings_buf_len = 6;
 
-    while (1)
-    {
-        gpio_set_level(ADXL_IO_INT1, 0);
+    xTaskCreate(task1, "task1", 4096, NULL, 10, &ISR);
 
-        vTaskDelay(20);
-        gpio_set_level(ADXL_IO_INT1, 1);
-        vTaskDelay(20);
-
-        // i2c_master_transmit_receive(master_dev_handle, read_data, read_data_len,
-        //                             readings_buf, readings_buf_len, 100);
-
-        // int16_t x_out, y_out, z_out;
-        // x_out = (readings_buf[0] | (readings_buf[1] << 8));
-        // y_out = (readings_buf[2] | (readings_buf[3] << 8));
-        // z_out = (readings_buf[4] | (readings_buf[5] << 8));
-        // printf("x_out: %d, y_out: %d, z_out: %d\n", x_out, y_out, z_out);
-
-        // vTaskDelay(200 / portTICK_PERIOD_MS);
-    }
 }
